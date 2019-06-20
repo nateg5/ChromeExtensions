5ad5ce02319c8ffdd9590fa4f0fbeb8143c38db9
ngardner <nate.gardner@tekcomms.com>
2019-06-20 12:37:18 -0500

    - added show number dropdown for selecting how many products to show

--- AmazonExtension/popup.js
+++ AmazonExtension/popup.js
@@ -2,18 +2,32 @@
 'use strict';
 
 let topRated = document.getElementById('topRated');
+let showNumber = document.getElementById('showNumber');
 
-chrome.storage.local.get('checked', function(item) {
-    document.getElementById('topRated').checked = item.checked;
+chrome.storage.local.get('props', function(item) {
+    topRated.checked = item.props.checked;
+    showNumber.value = item.props.showNumber;
 });
 
 topRated.onclick = function(element) {
+  update();
+};
+
+showNumber.onchange = function(element) {
+  update();
+};
+
+function update() {
   chrome.storage.local.set({
-      checked: element.target.checked
+      props: {
+        checked: topRated.checked,
+        showNumber: showNumber.value
+      }
   });
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     chrome.tabs.executeScript(
         tabs[0].id,
         {file: 'topRated.js'});
   });
-};
+
+}
--- AmazonExtension/topRated.js
+++ AmazonExtension/topRated.js
@@ -1,9 +1,30 @@
-chrome.storage.local.get('checked', function(item) {
-    checked = item.checked;
+chrome.storage.local.get('props', function(item) {
+    checked = item.props.checked;
+    showNumber = Number(item.props.showNumber);
     items = document.getElementsByClassName('zg-item-immersion');
     if(items.length === 0) {
         items = document.getElementsByClassName('s-result-item');
     }
+    let totalCount = 0;
+    for(let i = 0; i < items.length; i++) {
+        let count = 1;
+        let countArray = items[i].getElementsByClassName('a-size-small a-link-normal');
+        if(countArray.length === 0) {
+            countArray = items[i].getElementsByClassName('a-size-base');
+        }
+        if(countArray.length > 0) {
+            count = undefined;
+            for(let j = 0; j < countArray.length && isNaN(count); j++) {
+                count = countArray[j].innerHTML;
+                count = parseInt(count.replace(",", ""));
+            }
+        }
+        if(isNaN(count)) {
+            count = 1;
+        }
+        totalCount += count;
+    }
+    let weight = Math.ceil(totalCount / items.length / 10);
     itemsArray = [];
     for(let i = 0; i < items.length; i++) {
         let rating = 1;
@@ -33,17 +54,19 @@ chrome.storage.local.get('checked', function(item) {
         }
         itemsArray.push({
             item: items[i],
-            rating: (rating * count) / (count + 3)
+            rating: (rating * count) / (count + weight)
         });
     }
     itemsArray.sort(function(a, b) {
         return b.rating - a.rating;
     });
-    colorArray = ["#b0ffb0", "#c0ffc0", "#d0ffd0", "#e0ffe0", "#f0fff0"];
+    colorArray = ["#60ff60", "#70ff70", "#80ff80", "#90ff90", "#a0ffa0", "#b0ffb0", "#c0ffc0", "#d0ffd0", "#e0ffe0", "#f0fff0"];
     for(let i = 0; i < itemsArray.length; i++) {
         if(checked) {
-            if(i < 5) {
+            if(i < showNumber) {
                 itemsArray[i].item.style.backgroundColor = colorArray[i];
+                itemsArray[i].item.style.height = 'auto';
+                itemsArray[i].item.style.visibility = 'visible';
             } else {
                 itemsArray[i].item.style.height = '0px';
                 itemsArray[i].item.style.visibility = 'hidden';
