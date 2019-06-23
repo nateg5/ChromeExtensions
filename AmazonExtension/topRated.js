3c7aef67ce5e00bae0032c84b7efb2ddb1e7e5b2
Gardner <ngardner@netscout.com>
2019-06-23 12:00:01 -0500

    - added ranking number to items when filtered

--- AmazonExtension/topRated.js
+++ AmazonExtension/topRated.js
@@ -44,9 +44,10 @@ chrome.storage.local.get('props', function(item) {
         if(countArray.length === 0) {
             countArray = items[i].getElementsByClassName('a-size-base');
         }
+        let j = 0;
         if(countArray.length > 0) {
             count = undefined;
-            for(let j = 0; j < countArray.length && isNaN(count); j++) {
+            for(j = 0; j < countArray.length && isNaN(count); j++) {
                 count = countArray[j].innerHTML;
                 count = parseInt(count.replace(",", ""));
             }
@@ -57,6 +58,7 @@ chrome.storage.local.get('props', function(item) {
         }
         itemsArray.push({
             item: items[i],
+            countItem: countArray[j - 1],
             rating: (rating * count) / (count + weight)
         });
     }
@@ -65,19 +67,20 @@ chrome.storage.local.get('props', function(item) {
     });
     colorArray = ["#00ff00", "#10ff10", "#20ff20", "#30ff30", "#40ff40", "#50ff50", "#60ff60", "#70ff70", "#80ff80", "#90ff90", "#a0ffa0", "#b0ffb0", "#c0ffc0", "#d0ffd0", "#e0ffe0", "#f0fff0"];
     for(let i = 0; i < itemsArray.length; i++) {
+        itemsArray[i].item.style.backgroundColor = "#ffffff";
+        itemsArray[i].item.style.height = 'auto';
+        itemsArray[i].item.style.visibility = 'visible';
+        if(itemsArray[i].countItem.innerHTML.lastIndexOf(" - #") >= 0) {
+            itemsArray[i].countItem.innerHTML = itemsArray[i].countItem.innerHTML.substring(0, itemsArray[i].countItem.innerHTML.lastIndexOf(" - #"));
+        }
         if(checked) {
             if(i < showNumber) {
                 itemsArray[i].item.style.backgroundColor = colorArray[i];
-                itemsArray[i].item.style.height = 'auto';
-                itemsArray[i].item.style.visibility = 'visible';
+                itemsArray[i].countItem.innerHTML += " - #" + (i + 1);
             } else {
                 itemsArray[i].item.style.height = '0px';
                 itemsArray[i].item.style.visibility = 'hidden';
             } 
-        } else {
-            itemsArray[i].item.style.backgroundColor = "#ffffff";
-            itemsArray[i].item.style.height = 'auto';
-            itemsArray[i].item.style.visibility = 'visible';
         }
     }
 });
