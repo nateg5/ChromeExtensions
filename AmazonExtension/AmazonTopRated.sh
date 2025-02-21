9768e2e8a1bf3ea27e38e231bf7f43bbf912e4f2
nateg5 <nateg5@gmail.com>
2025-02-21 17:42:07 -0600

    - initial commit of automated testing shell script

---
+++ AmazonExtension/AmazonTopRated.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+for (( ; ; ))
+do
+
+	for (( ; ; ))
+	do
+	
+		sleep 3600
+		now=$(date +"%H%M%S");
+		
+		if [ $now -lt "010000" ]; then
+		
+			chromium "https://www.amazon.com/s?k=laptop&crid=1BONC12GPA73D&sprefix=laptop%2Caps%2C279" &
+			sleep 60
+			pkill chromium
+			break
+		
+		fi
+	
+	done
+
+	for (( ; ; ))
+	do
+	
+		sleep 3600
+		now=$(date +"%H%M%S");
+		
+		if [ $now -lt "010000" ]; then
+		
+			chromium "https://www.amazon.com/Best-Sellers-Computers-Accessories-Laptop-Computers/zgbs/pc/565108/ref=zg_bs_nav_pc_1" &
+			sleep 60
+			pkill chromium
+			break
+		
+		fi
+	
+	done
+	
+done
