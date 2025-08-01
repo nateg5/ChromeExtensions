a974846efa45ce6fa23346671af9b92b73fd4d0c
Nate Gardner <nateg5@gmail.com>
2025-08-01 15:55:03 -0500

    - moved common dte and premium code into a function

--- FidelityExtension/fidelity.js
+++ FidelityExtension/fidelity.js
@@ -22,29 +22,8 @@ chrome.storage.local.get("props", function (item) {
 		console.log("expirationText", expirationText);
 		console.log("premiumText", premiumText);
 		
-		if(selectedRow && selectedRow.children.length <= 11 && expirationText && premiumText) {
-			let today = new Date();
-			today.setHours(0);
-			today.setMinutes(0);
-			today.setSeconds(0);
-			let expirationDate = new Date(expirationText);
-			expirationDate.setHours(0);
-			expirationDate.setMinutes(0);
-			expirationDate.setSeconds(0);
-			
-			let dte = Math.round((expirationDate - today)/(24*60*60*1000)) + 1;
-			let premium = Number(premiumText.replace("$", "").replace(" ", ""));
-			
-			console.log("dte", dte);
-			console.log("premium", premium);
-			
-			let dteAdjustment = today.getDay() - 1;
-			let adjustedDTE = dte + dteAdjustment;
-			let tradingDTE = (Math.floor(adjustedDTE / 7) * 5) + (adjustedDTE % 7) - dteAdjustment;
-			
-			//tradingDTE -= (((new Date()).getHours() - 8) / 7);
-			
-			console.log("tradingDTE", tradingDTE);
+		if(selectedRow && selectedRow.children.length <= 11 && expirationText && premiumText) {			
+			let {dte, tradingDTE, premium} = getDTEsAndPremium(expirationText, premiumText);
 			
 			if(isNaN(dte) || isNaN(tradingDTE) || isNaN(premium)) {
 				selectedRow.children[2].innerHTML = "--";
@@ -82,28 +61,7 @@ chrome.storage.local.get("props", function (item) {
 			console.log("premiumText", premiumText);
 			
 			if(selectedRow && selectedRow.children.length <= 11 && expirationText && premiumText) {
-				let today = new Date();
-				today.setHours(0);
-				today.setMinutes(0);
-				today.setSeconds(0);
-				let expirationDate = new Date(expirationText);
-				expirationDate.setHours(0);
-				expirationDate.setMinutes(0);
-				expirationDate.setSeconds(0);
-				
-				let dte = Math.round((expirationDate - today)/(24*60*60*1000)) + 1;
-				let premium = Number(premiumText.replace("$", "").replace(" ", ""));
-				
-				console.log("dte", dte);
-				console.log("premium", premium);
-				
-				let dteAdjustment = today.getDay() - 1;
-				let adjustedDTE = dte + dteAdjustment;
-				let tradingDTE = (Math.floor(adjustedDTE / 7) * 5) + (adjustedDTE % 7) - dteAdjustment;
-				
-				//tradingDTE -= (((new Date()).getHours() - 8) / 7);
-				
-				console.log("tradingDTE", tradingDTE);
+				let {dte, tradingDTE, premium} = getDTEsAndPremium(expirationText, premiumText);
 				
 				if(isNaN(dte) || isNaN(tradingDTE) || isNaN(premium)) {
 					selectedRow.children[6].innerHTML = "--";
@@ -121,4 +79,30 @@ chrome.storage.local.get("props", function (item) {
 	}
 	/**/
   }, 1000);
+  let getDTEsAndPremium = (expirationText, premiumText) => {
+		let today = new Date();
+		today.setHours(0);
+		today.setMinutes(0);
+		today.setSeconds(0);
+		let expirationDate = new Date(expirationText);
+		expirationDate.setHours(0);
+		expirationDate.setMinutes(0);
+		expirationDate.setSeconds(0);
+		
+		let dte = Math.round((expirationDate - today)/(24*60*60*1000)) + 1;
+		let premium = Number(premiumText.replace("$", "").replace(" ", ""));
+		
+		console.log("dte", dte);
+		console.log("premium", premium);
+		
+		let dteAdjustment = today.getDay() - 1;
+		let adjustedDTE = dte + dteAdjustment;
+		let tradingDTE = (Math.floor(adjustedDTE / 7) * 5) + (adjustedDTE % 7) - dteAdjustment;
+		
+		//tradingDTE -= (((new Date()).getHours() - 8) / 7);
+		
+		console.log("tradingDTE", tradingDTE);
+		
+		return {dte, tradingDTE, premium};
+  };
 });
