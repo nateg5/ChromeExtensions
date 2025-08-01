a7ceea6cb5a80b1b3931f245dbe779a191552381
Nate Gardner <nateg5@gmail.com>
2025-08-01 15:45:25 -0500

    - fixed bug with calculating ppd - added support for option chain page

--- FidelityExtension/fidelity.js
+++ FidelityExtension/fidelity.js
@@ -5,6 +5,9 @@ chrome.storage.local.get("props", function (item) {
       return;
     }
 	
+	/**
+	 * Option Strategy Builder Page
+	 **/
 	let expirationText = document.getElementsByClassName("form-expiration")[0]?.getElementsByClassName("binding-val")[0]?.innerText || undefined;
 	if(expirationText) {
 		expirationText = expirationText.replace(" AM", "").replace(" PM", "");
@@ -19,7 +22,7 @@ chrome.storage.local.get("props", function (item) {
 		console.log("expirationText", expirationText);
 		console.log("premiumText", premiumText);
 		
-		if(selectedRow && expirationText && premiumText) {
+		if(selectedRow && selectedRow.children.length <= 11 && expirationText && premiumText) {
 			let today = new Date();
 			today.setHours(0);
 			today.setMinutes(0);
@@ -46,7 +49,7 @@ chrome.storage.local.get("props", function (item) {
 			if(isNaN(dte) || isNaN(tradingDTE) || isNaN(premium)) {
 				selectedRow.children[2].innerHTML = "--";
 			} else {
-				let ppd = (premium / tradingDTE) * 100;
+				let ppd = (premium * 100) / tradingDTE;
 				if(ppd > 10) {
 					ppd = Math.floor(ppd);
 				} else {
@@ -56,5 +59,66 @@ chrome.storage.local.get("props", function (item) {
 			}
 		}
 	}
+	/**/
+	
+	/**
+	 * Option Chain Page
+	 **/
+	let expirationDates = document.getElementsByClassName("expiration-date");
+	
+	if(expirationDates.length == 1) {
+		let expirationText = expirationDates[0]?.innerText || undefined;
+		if(expirationText && expirationText.indexOf("(W)") >= 0) {
+			expirationText = expirationText.replace("(W)", "");
+		}
+		
+		let gridRows = document.getElementsByClassName("ag-row ag-row-level-1") || undefined;
+		
+		for(let i = 0; gridRows && i < gridRows.length; i++) {
+			let selectedRow = gridRows[i] || undefined;
+			let premiumText = selectedRow?.getElementsByTagName("div")[5]?.innerText?.replace("Sell at ", "") || undefined;
+			
+			console.log("expirationText", expirationText);
+			console.log("premiumText", premiumText);
+			
+			if(selectedRow && selectedRow.children.length <= 11 && expirationText && premiumText) {
+				let today = new Date();
+				today.setHours(0);
+				today.setMinutes(0);
+				today.setSeconds(0);
+				let expirationDate = new Date(expirationText);
+				expirationDate.setHours(0);
+				expirationDate.setMinutes(0);
+				expirationDate.setSeconds(0);
+				
+				let dte = Math.round((expirationDate - today)/(24*60*60*1000)) + 1;
+				let premium = Number(premiumText.replace("$", "").replace(" ", ""));
+				
+				console.log("dte", dte);
+				console.log("premium", premium);
+				
+				let dteAdjustment = today.getDay() - 1;
+				let adjustedDTE = dte + dteAdjustment;
+				let tradingDTE = (Math.floor(adjustedDTE / 7) * 5) + (adjustedDTE % 7) - dteAdjustment;
+				
+				//tradingDTE -= (((new Date()).getHours() - 8) / 7);
+				
+				console.log("tradingDTE", tradingDTE);
+				
+				if(isNaN(dte) || isNaN(tradingDTE) || isNaN(premium)) {
+					selectedRow.children[6].innerHTML = "--";
+				} else {
+					let ppd = (premium * 100) / tradingDTE;
+					if(ppd > 10) {
+						ppd = Math.floor(ppd);
+					} else {
+						ppd = ppd.toFixed(2);
+					}
+					selectedRow.children[6].innerHTML = "$" + ppd + "/day";
+				}
+			}
+		}
+	}
+	/**/
   }, 1000);
 });
