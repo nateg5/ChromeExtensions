972db9b9afb1c713fb1db37fecbff089b739dd12
Nate Gardner <nateg5@gmail.com>
2026-03-02 14:27:04 -0600

    - added positions page section for calculating remaining premium per day for options

--- FidelityExtension/fidelity.js
+++ FidelityExtension/fidelity.js
@@ -19,9 +19,6 @@ chrome.storage.local.get("props", function (item) {
 		let selectedRow = gridRows[i] || undefined;
 		let premiumText = selectedRow?.getElementsByTagName("div")[1]?.innerText || undefined;
 		
-		console.log("expirationText", expirationText);
-		console.log("premiumText", premiumText);
-		
 		if(selectedRow && selectedRow.children.length <= 11 && expirationText && premiumText) {			
 			let {dte, tradingDTE, premium} = getDTEsAndPremium(expirationText, premiumText);
 			
@@ -57,9 +54,6 @@ chrome.storage.local.get("props", function (item) {
 			let selectedRow = gridRows[i] || undefined;
 			let premiumText = selectedRow?.getElementsByTagName("div")[5]?.innerText?.replace("Sell at ", "") || undefined;
 			
-			console.log("expirationText", expirationText);
-			console.log("premiumText", premiumText);
-			
 			if(selectedRow && selectedRow.children.length <= 11 && expirationText && premiumText) {
 				let {dte, tradingDTE, premium} = getDTEsAndPremium(expirationText, premiumText);
 				
@@ -78,6 +72,55 @@ chrome.storage.local.get("props", function (item) {
 		}
 	}
 	/**/
+	
+	/**
+	 * Positions Page
+	 **/
+	let titleElement = document.getElementsByClassName("portfolio-dashboard__container-content--title")[0];
+	if(titleElement && (titleElement.innerHTML.indexOf("SPY Options") >= 0 || titleElement.innerHTML.indexOf("Stock Options") >= 0)) {
+		let leftRows = document.getElementsByClassName("ag-pinned-left-cols-container")[0]?.getElementsByClassName("ag-row");
+		let rightRows = document.getElementsByClassName("ag-center-cols-container")[0]?.getElementsByClassName("ag-row");
+		
+		let optionIndexes = [];
+		let optionDates = [];
+		for(let i = 0; leftRows && i < leftRows.length; i++) {
+			let description = leftRows[i].getElementsByClassName("posweb-cell-symbol-description")[0];
+			if(description) {
+				let date = new Date(description.innerHTML);
+				if(date != "Invalid Date") {
+					optionIndexes.push(i);
+					optionDates.push(description.innerHTML);
+				}
+			}
+		}
+		
+		for(let i = 0; rightRows && i < optionIndexes.length; i++) {
+			let rowIndex = optionIndexes[i];
+			let row = rightRows[rowIndex];
+			
+			let curVal = row.querySelector("[col-id='curVal']").innerText;
+			let qty = row.querySelector("[col-id='qty']").innerText;
+			let totGLPct = row.querySelector("[col-id='totGLPct']").innerText;
+			let fifTwo = row.querySelector("[col-id='fifTwo']");
+			
+			curVal = Number(curVal.replace("$", ""));
+			qty = Number(qty);
+			let {dte, tradingDTE} = getDTEsAndPremium(optionDates[i], "");
+	
+			let remainingPPD = calculatePPD((curVal / qty) / 100, tradingDTE);
+			
+			totGLPct = Number(totGLPct.replace("+", "").replace("%", ""));
+			
+			let backgroundColor = "#ffffff";
+			if(totGLPct >= 50) {
+				backgroundColor = "#cde1c9";
+			}
+			
+			fifTwo.getElementsByClassName("posweb-cell-fifty_two_week_range_container")[0].innerText = "$" + remainingPPD + "/day";
+			fifTwo.style.backgroundColor = backgroundColor;
+		}
+	}
+	/**/
   }, 1000);
   let getDTEsAndPremium = (expirationText, premiumText) => {
 		let today = new Date();
@@ -92,16 +135,11 @@ chrome.storage.local.get("props", function (item) {
 		let dte = Math.round((expirationDate - today)/(24*60*60*1000)) + 1;
 		let premium = Number(premiumText.replace("$", "").replace(" ", ""));
 		
-		console.log("dte", dte);
-		console.log("premium", premium);
-		
 		let dteAdjustment = today.getDay() - 1;
 		let adjustedDTE = dte + dteAdjustment;
 		let tradingDTE = (Math.floor(adjustedDTE / 7) * 5) + (adjustedDTE % 7) - dteAdjustment;
 		
 		//tradingDTE -= (((new Date()).getHours() - 8) / 7);
-		
-		console.log("tradingDTE", tradingDTE);
 		
 		return {dte, tradingDTE, premium};
   };
